506b829ac3d6e85c6e106158fcbdd3e141098e59
ismailsekerci <ismailsekerci@gmail.com>
2024-09-06 15:14:33 +0300

    Category and Product relation is ready

---
+++ Foody.PresentationLayer/ViewComponents/AdminLayoutViewComponents/_FooterLayoutComponenetPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.PresentationLayer.ViewComponents.AdminLayoutViewComponents
+{
+	public class _FooterLayoutComponenetPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
